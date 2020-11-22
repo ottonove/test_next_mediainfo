7fe0e7d8a4e3f48270b5e850a69712967a7cce4c
ottonove <niigata01web@gmail.com>
2020-11-23 01:18:31 +0900

    CDNから追加したmediainfo.jsが動作した。

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,64 @@
+// import { MediaInfo } from 'mediainfo.js'
 import Head from 'next/head'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  useEffect(() => {
+    /* MediaInfo().then((mediainfo) => {
+      mediainfo
+      .analyzeData(() => file.size, readChunk(file))
+      .then((result) => {
+        console.log('result:', result);
+      })
+    }); */
+  })
+
+  const onChangeFile = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      // output.value = 'Working…'
+  
+      const getSize = () => file.size
+  
+      const readChunk = (file) => (chunkSize, offset) =>
+        new Promise((resolve, reject) => {
+          const reader = new FileReader()
+          reader.onload = (event) => {
+            if (event.target.error) {
+              reject(event.target.error)
+            }
+            resolve(new Uint8Array(event.target.result))
+          }
+          reader.readAsArrayBuffer(file.slice(offset, offset + chunkSize))
+        })
+  
+        MediaInfo().then((mediainfo) => {
+          mediainfo
+          .analyzeData(getSize, readChunk(file))
+          .then((result) => {
+            console.log('result:', result);
+          })
+        });
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <script type="text/javascript" src="https://unpkg.com/mediainfo.js/dist/mediainfo.min.js"></script>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <div>
+          <input type="file" onChange={onChangeFile} />
+        </div>
 
         <p className={styles.description}>
           Get started by editing{' '}
