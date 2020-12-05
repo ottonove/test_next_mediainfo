41d9103938fc5018bf10fef1c9b3d3eff2c590c8
ottonove <niigata01web@gmail.com>
2020-12-05 20:24:49 +0900

    マーカーARを試した。 .pattファイルが大事。

---
+++ pages/ar01.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <iframe
+      src="static/ar01_src.html"
+      width="1200px"
+      height="800px"
+    >
+    </iframe>
+  )
+}
---
+++ pages/ar02.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <iframe
+        src="static/NG2_ar02_src.html"
+        width="1200px"
+        height="800px"
+      >
+      </iframe>
+    )
+  }
